205a686b690d2384bc974ac86dc4e05f08ed02ea
PHC <lucidsun92@gmail.com>
2022-03-11 17:27:26 +0900

    test1
    backgroundworker with progress and cancel while working

---
+++ WinformTest/WinformTestSol/TestProj1/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace TestProj1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.bgw = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(36, 372);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(255, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // bgw
+            // 
+            this.bgw.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgw_DoWork);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(36, 280);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(732, 43);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(309, 105);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(38, 12);
+            this.resultLabel.TabIndex = 2;
+            this.resultLabel.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button2.Location = new System.Drawing.Point(513, 372);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(255, 66);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Cancel!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker bgw;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WinformTest/WinformTestSol/TestProj1/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestProj1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeBackgroundWorker();
+        }
+
+        private void InitializeBackgroundWorker()
+        {
+            bgw.WorkerReportsProgress = true;
+            bgw.WorkerSupportsCancellation = true;
+
+            bgw.DoWork +=
+                new DoWorkEventHandler(bgw_DoWork);
+            bgw.RunWorkerCompleted +=
+                new RunWorkerCompletedEventHandler(
+            bgw_RunWorkerCompleted);
+            bgw.ProgressChanged +=
+                new ProgressChangedEventHandler(
+            bgw_ProgressChanged);
+        }
+
+        private void bgw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            for (int i = 0; i <= 100; ++i)
+            {
+                if (bgw.CancellationPending)
+                {
+                    e.Cancel = true;    // Will get e.Cancelled == true at line: 65
+                }
+                else
+                {
+                    System.Threading.Thread.Sleep(50);
+                    worker.ReportProgress(i);
+                }
+            }
+        }
+
+        private void bgw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            this.progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void bgw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.Message);
+            }
+            else if (e.Cancelled)
+            {
+                // CancelAsync was called.
+                resultLabel.Text = "Canceled";
+            }
+            else
+            {
+                // succeeded.
+                resultLabel.Text = e.Result.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(!bgw.IsBusy)
+                bgw.RunWorkerAsync();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(bgw.WorkerSupportsCancellation)
+                bgw.CancelAsync();
+        }
+    }
+}
